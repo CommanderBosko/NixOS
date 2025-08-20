5584c802db87f954ffe36798a446f6e9822fff13
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-20 14:32:50 -0400

    flake cleanup

--- dotfiles/venom/bootloader.nix
+++ dotfiles/venom/bootloader.nix
@@ -8,9 +8,9 @@
       grub = {
         enable = true;
         efiSupport = true;
-        efiInstallAsRemovable = true; # fallback EFI boot
-        useOSProber = true;           # detect Windows/Linux
-        device = "nodev";             # don't tie to a specific disk
+        efiInstallAsRemovable = true;
+        useOSProber = true;
+        device = "nodev";
       };
     };
   };
--- flake.nix
+++ flake.nix
@@ -2,23 +2,23 @@
   description = "Bosko's NixOS Flake";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    bosko.url = "github:CommanderBosko/NixOS?ref=main";
+    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    boskoRepo.url = "github:CommanderBosko/NixOS?ref=main";
   };
 
-  outputs = { self, nixpkgs, bosko, ... }@inputs: {
-    nixosConfigurations.venom = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgsStable, nixpkgsUnstable, boskoRepo, ... }@inputs: {
+    nixosConfigurations.venom = nixpkgsUnstable.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        "${bosko}/dotfiles/venom/bootloader.nix"
-        "${bosko}/dotfiles/venom/enviornment.nix"
+        "${boskoRepo}/dotfiles/venom/bootloader.nix"
+        "${boskoRepo}/dotfiles/venom/enviornment.nix"
         "/etc/nixos/hardware-configuration.nix"
-        "${bosko}/dotfiles/venom/networking.nix"
-        "${bosko}/dotfiles/venom/nvidia.nix"
-        "${bosko}/dotfiles/venom/users.nix"
-        "${bosko}/dotfiles/venom/virtualisation.nix"
+        "${boskoRepo}/dotfiles/venom/networking.nix"
+        "${boskoRepo}/dotfiles/venom/nvidia.nix"
+        "${boskoRepo}/dotfiles/venom/users.nix"
+        "${boskoRepo}/dotfiles/venom/virtualisation.nix"
       ];
     };
   };
