13621f0f19dc4849c244f9b9d92017fac2b50426
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-20 15:12:00 -0400

    flake

--- dotfiles/venom/bootloader.nix
+++ dotfiles/venom/bootloader.nix
@@ -1,27 +1,15 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
-let
-  # Detect if /boot exists and is a FAT32 partition (ESP)
-  hasESP = lib.mkDefault (
-    (config.fileSystems ? "/boot") &&
-    (config.fileSystems."/boot".fsType or null) == "vfat"
-  );
-in
 {
+  # Bootloader.
   boot = {
     kernelPackages = pkgs.linuxPackages_zen;
-
-    loader.grub = {
-      enable = true;
-      useOSProber = true;
-
-      # BIOS boot (always works on ext4 root, no ESP required)
-      device = lib.mkIf (!hasESP) (builtins.elemAt (builtins.split "/" config.fileSystems."/".device) 2);
-
-      # UEFI boot (only if /boot is vfat)
-      efiSupport = lib.mkIf hasESP true;
-      efiInstallAsRemovable = lib.mkIf hasESP true;
-      device = lib.mkIf hasESP "nodev"; # don’t clobber MBR
+    loader = {
+      grub = {
+        enable = true;
+        device = "/dev/nvme0n1";
+        useOSProber = true;
+      };
     };
   };
 }
