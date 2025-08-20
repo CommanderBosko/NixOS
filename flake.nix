bd1a76e4f2630fa33451c3a524696eb75b686c4a
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-20 17:06:31 -0400

    organization

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Bosko's NixOS Flake";
 
   inputs = {
-    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-25.05";
+#     nixpkgsStable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
     bosko.url = "github:CommanderBosko/NixOS?ref=main";
   };
 
-  outputs = { self, nixpkgsStable, nixpkgsUnstable, bosko, ... }@inputs:
+  outputs = { self, nixpkgsUnstable, bosko, ... }@inputs:
     let
       system = "x86_64-linux";
       host = "venom";
@@ -23,13 +23,13 @@
           inherit host;
         };
         modules = [
-          "${bosko}/dotfiles/gaming/bootloader.nix"
-          "${bosko}/dotfiles/gaming/enviornment.nix"
           "/etc/nixos/hardware-configuration.nix"
-          "${bosko}/dotfiles/gaming/networking.nix"
-          "${bosko}/dotfiles/gaming/nvidia.nix"
+          "${bosko}/dotfiles/common/bootloader.nix"
+          "${bosko}/dotfiles/common/networking.nix"
+          "${bosko}/dotfiles/common/nvidia.nix"
+          "${bosko}/dotfiles/common/virtualisation.nix"
+          "${bosko}/dotfiles/gaming/enviornment.nix"
           "${bosko}/dotfiles/gaming/users.nix"
-          "${bosko}/dotfiles/gaming/virtualisation.nix"
         ];
       };
     };
