659614a41e8e3e71d781d19e25160ef99dcafdc3
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-20 15:28:28 -0400

    flake

--- dotfiles/venom/bootloader.nix
+++ dotfiles/venom/bootloader.nix
@@ -1,15 +1,48 @@
-{ config, pkgs, ... }:
-
-{
-  # Bootloader.
+# BOOT related stuff
   boot = {
-    kernelPackages = pkgs.linuxPackages_zen;
-    loader = {
-      grub = {
-        enable = true;
-        device = "/dev/nvme0n1";
-        useOSProber = true;
-      };
+    kernelPackages = pkgs.linuxPackages_zen; # zen Kernel
+    #kernelPackages = pkgs.linuxPackages_latest; # Kernel
+
+    kernelParams = [
+      "systemd.mask=systemd-vconsole-setup.service"
+      "systemd.mask=dev-tpmrm0.device" #this is to mask that stupid 1.5 mins systemd bug
+      "nowatchdog"
+      "modprobe.blacklist=sp5100_tco" #watchdog for AMD
+      "modprobe.blacklist=iTCO_wdt" #watchdog for Intel
+ 	  ];
+
+    # This is for OBS Virtual Cam Support
+    #kernelModules = [ "v4l2loopback" ];
+    #  extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+      kernelModules = [ ];
     };
-  };
-}
+
+    # Needed For Some Steam Games
+    #kernel.sysctl = {
+    #  "vm.max_map_count" = 2147483642;
+    #};
+
+    ## BOOT LOADERS: NOTE USE ONLY 1. either systemd or grub
+    # Bootloader SystemD
+    loader.systemd-boot.enable = true;
+
+    loader.efi = {
+	    #efiSysMountPoint = "/efi"; #this is if you have separate /efi partition
+	    canTouchEfiVariables = true;
+  	  };
+
+    loader.timeout = 5;
+
+    # Bootloader GRUB
+    #loader.grub = {
+	    #enable = true;
+	    #  devices = [ "nodev" ];
+	    #  efiSupport = true;
+      #  gfxmodeBios = "auto";
+	    #  memtest86.enable = true;
+	    #  extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
+	    #  configurationName = "${host}";
+  	  #	 };
