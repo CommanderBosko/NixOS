34f4644e9d6d185588beb68aa031c3f11e2f6365
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-20 16:13:52 -0400

    flake

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         modules = [
           "${bosko}/dotfiles/gaming/bootloader.nix"
           "${bosko}/dotfiles/gaming/enviornment.nix"
-#           "/etc/nixos/hardware-configuration.nix"
+          "/etc/nixos/hardware-configuration.nix"
           "${bosko}/dotfiles/gaming/networking.nix"
           "${bosko}/dotfiles/gaming/nvidia.nix"
           "${bosko}/dotfiles/gaming/users.nix"
