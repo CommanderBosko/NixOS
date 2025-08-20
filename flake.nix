a5e2bfe46ef34b616af249f8d7b0db73cf5c1de7
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-20 16:02:05 -0400

    flake

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     in
     {
     nixosConfigurations = {
-        "${host}" = nixpkgsUnstable.lib.nixosSystem {
+        gaming = nixpkgsUnstable.lib.nixosSystem {
         specialArgs = {
           inherit inputs;
           inherit system;
@@ -23,13 +23,13 @@
           inherit host;
         };
         modules = [
-          "${bosko}/dotfiles/${host}/bootloader.nix"
-          "${bosko}/dotfiles/${host}/enviornment.nix"
+          "${bosko}/dotfiles/gaming/bootloader.nix"
+          "${bosko}/dotfiles/gaming/enviornment.nix"
           "/etc/nixos/hardware-configuration.nix"
-          "${bosko}/dotfiles/${host}/networking.nix"
-          "${bosko}/dotfiles/${host}/nvidia.nix"
-          "${bosko}/dotfiles/${host}/users.nix"
-          "${bosko}/dotfiles/${host}/virtualisation.nix"
+          "${bosko}/dotfiles/gaming/networking.nix"
+          "${bosko}/dotfiles/gaming/nvidia.nix"
+          "${bosko}/dotfiles/gaming/users.nix"
+          "${bosko}/dotfiles/gaming/virtualisation.nix"
         ];
       };
     };
