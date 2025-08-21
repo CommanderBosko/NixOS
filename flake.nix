b0adb676b0cb59cdc1755cbfa8b49a33a46ea66c
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-21 15:29:40 -0400

    organization

--- flake.nix
+++ flake.nix
@@ -2,34 +2,35 @@
   description = "Bosko's NixOS Flake";
 
   inputs = {
-#     nixpkgsStable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    bosko.url = "github:CommanderBosko/NixOS?ref=main";
+    boskoRepo.url = "github:CommanderBosko/NixOS?ref=main";
   };
 
-  outputs = { self, nixpkgsUnstable, bosko, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      host = "venom";
-      username = "bosko";
-    in
-    {
+  outputs = { self, nixpkgsStable, nixpkgsUnstable, boskoRepo, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    host = "venom";
+    username = "bosko";
+  in
+  {
     nixosConfigurations = {
         gaming = nixpkgsUnstable.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
-          inherit system;
-          inherit username;
-          inherit host;
-        };
+          specialArgs = {
+            inherit inputs;
+            inherit system;
+            inherit username;
+            inherit host;
+          };
+
         modules = [
           "/etc/nixos/hardware-configuration.nix"
-          "${bosko}/dotfiles/common/bootloader.nix"
-          "${bosko}/dotfiles/common/networking.nix"
-          "${bosko}/dotfiles/common/nvidia.nix"
-          "${bosko}/dotfiles/common/virtualisation.nix"
-          "${bosko}/dotfiles/gaming/enviornment.nix"
-          "${bosko}/dotfiles/gaming/users.nix"
+          "${boskoRepo}/dotfiles/common/bootloader.nix"
+          "${boskoRepo}/dotfiles/common/networking.nix"
+          "${boskoRepo}/dotfiles/common/nvidia.nix"
+          "${boskoRepo}/dotfiles/common/virtualisation.nix"
+          "${boskoRepo}/dotfiles/gaming/enviornment.nix"
+          "${boskoRepo}/dotfiles/gaming/users.nix"
         ];
       };
     };
