34f121ce6631d35cfa5dc542b9ec1c6af5b36578
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-21 16:51:27 -0400

    flake

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgsStable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    boskoRepo.url = "github:CommanderBosko/NixOS?ref=main";
+#     boskoRepo.url = "github:CommanderBosko/NixOS?ref=main";
   };
 
-  outputs = { self, nixpkgsStable, nixpkgsUnstable, boskoRepo, ... }@inputs:
+  outputs = { self, nixpkgsStable, nixpkgsUnstable, ... }@inputs:
   let
     system = "x86_64-linux";
     host = "venom";
