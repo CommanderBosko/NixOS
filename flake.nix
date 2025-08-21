82a765f60e489e4a3d0a4bbf25e73e9efaf732e4
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-21 15:48:21 -0400

    organization

--- flake.nix
+++ flake.nix
@@ -25,12 +25,12 @@
 
         modules = [
           "/etc/nixos/hardware-configuration.nix"
-          "${boskoRepo}/dotfiles/common/bootloader.nix"
-          "${boskoRepo}/dotfiles/common/networking.nix"
-          "${boskoRepo}/dotfiles/common/nvidia.nix"
-          "${boskoRepo}/dotfiles/common/virtualisation.nix"
-          "${boskoRepo}/dotfiles/gaming/enviornment.nix"
-          "${boskoRepo}/dotfiles/gaming/users.nix"
+          "./dotfiles/common/bootloader.nix"
+          "./dotfiles/common/networking.nix"
+          "./dotfiles/common/nvidia.nix"
+          "./dotfiles/common/virtualisation.nix"
+          "./dotfiles/gaming/enviornment.nix"
+          "./dotfiles/gaming/users.nix"
         ];
       };
     };
