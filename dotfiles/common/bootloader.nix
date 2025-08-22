459aece214b1b12492be1929677e7caee871f989
Kurt Hoernig <kurthoernig@gmail.com>
2025-08-22 17:55:09 -0400

    universal bootloader

--- dotfiles/common/bootloader.nix
+++ dotfiles/common/bootloader.nix
@@ -1,6 +1,34 @@
 { config, pkgs, ... }:
 
 {
+  # Partition drives
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1"; # YOUR DRIVE HERE exampe: "/dev/sda"
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "512MiB";
+          type = "EF00"; # EFI system partition
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        };
+        root = {
+          size = "100%"; # use rest of the disk
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+  };
+
   # Bootloader.
   boot = {
     kernelPackages = pkgs.linuxPackages_zen;
