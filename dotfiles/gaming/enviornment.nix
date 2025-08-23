09dea10604b890d06da915d295f3a7303bd24664
CommanderBosko <kurthoernig@gmail.com>
2025-08-22 21:57:12 -0400

    split networking.nix

--- dotfiles/gaming/enviornment.nix
+++ dotfiles/gaming/enviornment.nix
@@ -143,7 +143,7 @@
       shellAliases = {
         ll = "ls -l";
         edit = "sudo micro";
-        update = "sudo nix flake update --flake ~/NixOS/. && sudo nixos-rebuild switch --flake ~/NixOS#gaming --impure && flatpak update";
+        update = "sudo nix flake update --flake ~/NixOS/. && sudo nixos-rebuild switch --flake ~/NixOS#gaming --impure && flatpak update -y";
         ".." = "cd ..";
         "/" = "cd /";
         "~" = "cd ~";
