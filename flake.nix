526e4a50c57c333a3f15a44e9a7cd8a1cb78faf3
CommanderBosko <kurthoernig@gmail.com>
2025-08-27 15:21:38 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,35 +11,48 @@
     system = "x86_64-linux";
     host = "venom";
     username = "bosko";
+
+    # Import nixpkgs for the package sets
+#     pkgsStable = import nixpkgsStable { inherit system; config.allowUnfree = true; };
+#     pkgsUnstable = import nixpkgsUnstable */{ inherit system; config.allowUnfree = true; };
   in
   {
+    # Add non-nix packages
+    packages.${system} = {
+      rift = import ./dotfiles/gaming/rift.nix { pkgs = nixpkgsUnstable; };
+    };
+
+    # Configure nix configurations
     nixosConfigurations = {
-        gaming = nixpkgsUnstable.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs;
-            inherit system;
-            inherit username;
-            inherit host;
-          };
+      # Gaming with KDE
+      gaming = nixpkgsUnstable.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+          inherit system;
+          inherit username;
+          inherit host;
+        };
 
-        modules = [
-          "/etc/nixos/hardware-configuration.nix"
-          "${self}/dotfiles/common/bootloader.nix"
-          "${self}/dotfiles/common/nvidia.nix"
-          "${self}/dotfiles/common/users.nix"
-          "${self}/dotfiles/common/virtualisation.nix"
-          "${self}/dotfiles/gaming/enviornment.nix"
-          "${self}/dotfiles/gaming/networking.nix"
-        ];
-      };
+      modules = [
+        "/etc/nixos/hardware-configuration.nix"
+        "${self}/dotfiles/common/bootloader.nix"
+        "${self}/dotfiles/common/nvidia.nix"
+        "${self}/dotfiles/common/users.nix"
+        "${self}/dotfiles/common/virtualisation.nix"
+        "${self}/dotfiles/gaming/enviornment.nix"
+        "${self}/dotfiles/gaming/networking.nix"
+        "${self}/dotfiles/gaming/rift.nix"
+      ];
+    };
 
+      # Laptop
       laptop = nixpkgsStable.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs;
-            inherit system;
-            inherit username;
-            inherit host;
-          };
+        specialArgs = {
+          inherit inputs;
+          inherit system;
+          inherit username;
+          inherit host;
+        };
 
         modules = [
           "/etc/nixos/hardware-configuration.nix"
