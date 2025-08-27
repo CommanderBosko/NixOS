2f415c3178472d9ff356dc20dbf10dfbdaa3a7fc
CommanderBosko <kurthoernig@gmail.com>
2025-08-27 14:53:51 -0400

    working on rift intel on gaming

--- dotfiles/gaming/rift.nix
+++ dotfiles/gaming/rift.nix
@@ -1,51 +1,38 @@
-{ pkgs, ... }:
+{ stdenv, fetchurl, dpkg }:
 
-{
-  environment.systemPackages = [
-    (pkgs.stdenv.mkDerivation {
-      pname = "rift";
-      version = "5.1.2";
+stdenv.mkDerivation rec {
+  pname = "rift";
+  version = "5.1.2";
 
-      src = pkgs.fetchurl {
-        url = "https://riftforeve.online/download/debian/rift_5.1.2_amd64.deb";
-        sha256 = "1af54h6j7mpaknyxydqi9aiz6g31kfwcllknhqzpv96bn2ps31lj";
-      };
+  src = fetchurl {
+    url = "https://riftforeve.online/download/debian/rift_5.1.2_amd64.deb";
+    sha256 = "1af54h6j7mpaknyxydqi9aiz6g31kfwcllknhqzpv96bn2ps31lj";
+  };
 
-      nativeBuildInputs = [ pkgs.dpkg pkgs.makeWrapper ];
+  nativeBuildInputs = [ dpkg ];
 
-      unpackPhase = ''
-        dpkg-deb -x $src .
-      '';
+  dontBuild = true;
+  dontConfigure = true;
 
-      installPhase = ''
-        mkdir -p $out
+  installPhase = ''
+    runHook preInstall
 
-        # Copy everything from .deb into $out
-        cp -r usr/* $out/
+    mkdir -p $out
+    dpkg-deb -x $src $out
 
-        # Ensure the binary is wrapped so it finds its libraries
-        mkdir -p $out/bin
-        makeWrapper $out/bin/rift $out/bin/rift \
-          --prefix LD_LIBRARY_PATH : $out/lib
+    mkdir -p $out/bin
+    if [ -f $out/opt/rift/rift ]; then
+      cp $out/opt/rift/rift $out/bin/rift
+      chmod +x $out/bin/rift
+    fi
 
-        # Install desktop entry
-        mkdir -p $out/share/applications
-        cat > $out/share/applications/rift.desktop <<EOF
-        [Desktop Entry]
-        Name=Rift
-        Exec=$out/bin/rift
-        Icon=rift
-        Type=Application
-        Categories=Game;
-        EOF
+    runHook postInstall
+  '';
 
-        # Install icon (if available in the deb)
-        if [ -f $out/share/icons/hicolor/256x256/apps/rift.png ]; then
-          mkdir -p $out/share/icons/hicolor/256x256/apps
-          cp $out/share/icons/hicolor/256x256/apps/rift.png \
-             $out/share/icons/hicolor/256x256/apps/rift.png
-        fi
-      '';
-    })
-  ];
+  meta = with stdenv.lib; {
+    description = "Rift Intel Tool";
+    homepage = "https://riftforeve.online";
+    license = licenses.unfree;
+    platforms = [ "x86_64-linux" ];
+  };
 }
