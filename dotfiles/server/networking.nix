95bef8960732b96212247b3c5c21f8ff6672f25b
CommanderBosko <kurthoernig@gmail.com>
2025-09-07 19:25:56 -0400

    added server setup

--- dotfiles/server/networking.nix
+++ dotfiles/server/networking.nix
@@ -24,9 +24,9 @@
     # services.openssh.enable = true;
 
     # Open ports in the firewall.
-    networking.firewall.allowedTCPPorts = [ 22 ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
+    firewall.allowedTCPPorts = [ 22 ];
+    # firewall.allowedUDPPorts = [ ... ];
     # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
+    firewall.enable = true;
   };
 }
