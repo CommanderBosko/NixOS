1c3e9803cb520825899e7188bb0d795451b25364
CommanderBosko <kurthoernig@gmail.com>
2025-09-07 20:30:14 -0400

    networking

--- dotfiles/server/networking.nix
+++ dotfiles/server/networking.nix
@@ -11,7 +11,13 @@
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     # Enable networking
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
+
+    # Ename custom DNS servers
+    nameservers = [ "10.0.0.20" "1.1.1.1" ];
 
     # Enable and open ports in the firewall
     firewall = {
