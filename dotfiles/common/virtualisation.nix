f1baebb864c3e05a1275d470ba64e908c502c2a0
CommanderBosko <kurthoernig@gmail.com>
2025-09-07 21:55:42 -0400

    organization

--- dotfiles/common/virtualisation.nix
+++ dotfiles/common/virtualisation.nix
@@ -1,42 +1,45 @@
 { config, pkgs, ... }:
 
 {
-  # Enable virtualisation
+  # Enable libvirt with QEMU/KVM
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       swtpm.enable = true;
       ovmf = {
         enable = true;
-        packages = with pkgs; [
-          OVMFFull.fd
-        ];
+        packages = with pkgs; [ OVMFFull.fd ];
       };
+      runAsRoot = false;
     };
   };
 
-  # Configure "default" network
-  environment = {
-    etc."libvirt/qemu/networks/default.xml".text = ''
-      <network>
-        <name>default</name>
-        <forward mode="nat"/>
-        <ip address="192.168.122.1" netmask="255.255.255.0">
-          <dhcp>
-            <range start="192.168.122.2" end="192.168.122.254"/>
-          </dhcp>
-        </ip>
-      </network>
-    '';
+  # Networking
+  environment.etc."libvirt/qemu/networks/default.xml".text = ''
+    <network>
+      <name>default</name>
+      <forward mode="nat"/>
+      <ip address="192.168.122.1" netmask="255.255.255.0">
+        <dhcp>
+          <range start="192.168.122.2" end="192.168.122.254"/>
+        </dhcp>
+      </ip>
+    </network>
+  '';
 
-    # Virtualisation packages
-    systemPackages = with pkgs; [
-      virt-manager
-      virt-viewer
-      spice
-      spice-gtk
-      spice-protocol
-      qemu
-    ];
-  };
+  # Packages
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice
+    spice-gtk
+    spice-protocol
+    qemu
+  ];
+
+  # Required for virt-manager auth
+  security.polkit.enable = true;
+
+  # Enable dconf for virt-manager settings
+  programs.dconf.enable = true;
 }
