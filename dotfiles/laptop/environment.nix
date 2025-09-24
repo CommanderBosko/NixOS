2f2f2a5193c5d6e1fc4594ee665f4b339a0f22cb
CommanderBosko <kurthoernig@gmail.com>
2025-09-24 17:15:56 -0400

    starship.toml added

--- dotfiles/laptop/environment.nix
+++ dotfiles/laptop/environment.nix
@@ -133,13 +133,13 @@
   ];
 
   # Enable Starship
-    programs.starship.enable = true;
-    system.activationScripts.starship = {
-      text = ''
-        mkdir -p ~/.config
-        cp /home/bosko/NixOS/dotfiles/common/starship.toml /home/bosko/.config/starship.toml
-      '';
-    };
+  programs.starship.enable = true;
+  system.activationScripts.starship = {
+    text = ''
+      mkdir -p ~/.config
+      cp /home/bosko/NixOS/dotfiles/common/starship.toml /home/bosko/.config/starship.toml
+    '';
+  };
 
   # Programs
   programs = {
