829a69118fb6704ae25d03246e688eca162f4b24
CommanderBosko <kurthoernig@gmail.com>
2025-11-04 23:03:59 -0500

    added user natalie

--- dotfiles/common/users.nix
+++ dotfiles/common/users.nix
@@ -1,7 +1,4 @@
-{ config, pkgs, ... }:
-
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.bosko = {
     shell = pkgs.zsh;
     isNormalUser = true;
@@ -13,11 +10,22 @@
       "wheel"
     ];
 
-    # User specific packages
-    packages = with pkgs; [
-    ];
+    packages = with pkgs; [ ];
   };
 
-  # Define trusted users
-  nix.settings.trusted-users = [ "bosko" ];
+  users.users.natty = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    description = "natty";
+    extraGroups = [
+      "kvm"
+      "libvirtd"
+      "networkmanager"
+      "wheel"
+    ];
+
+    packages = with pkgs; [ ];
+  };
+
+  nix.settings.trusted-users = [ "bosko" "natty" ];
 }
