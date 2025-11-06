568bef86abb1767ec44a6f0d357dbb6cb6ee6785
CommanderBosko <kurthoernig@gmail.com>
2025-11-06 09:02:43 -0500

    added user to individual environments

---
+++ dotfiles/natalie/users.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  # Definie user
+  users.users.natty = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    description = "natty";
+    extraGroups = [
+      "kvm"
+      "libvirtd"
+      "networkmanager"
+      "wheel"
+    ];
+
+    packages = with pkgs; [ ];
+  };
+
+  nix.settings.trusted-users = [ "natty" ];
+}
--- dotfiles/server/environment.nix
+++ dotfiles/server/environment.nix
@@ -1,23 +1,6 @@
 { config, pkgs, system, ... }:
 
 {
-  # Definie user
-  users.users.bosko = {
-    shell = pkgs.zsh;
-    isNormalUser = true;
-    description = "bosko";
-    extraGroups = [
-      "kvm"
-      "libvirtd"
-      "networkmanager"
-      "wheel"
-    ];
-
-    packages = with pkgs; [ ];
-  };
-
-  nix.settings.trusted-users = [ "bosko" ];
-
   # System
   system = {
     # Set NixOS Version
