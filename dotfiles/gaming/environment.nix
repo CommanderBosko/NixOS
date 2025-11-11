a49e9c40fadd06d8805dd9dd5165ffc84f8cbbb4
CommanderBosko <kurthoernig@gmail.com>
2025-11-10 21:53:54 -0500

    added alias for albion online

--- dotfiles/gaming/environment.nix
+++ dotfiles/gaming/environment.nix
@@ -133,6 +133,7 @@
         update = "sudo nix flake update --flake ~/NixOS/. && flatpak update -y && flatpak remove --unused --noninteractive && sudo nixos-rebuild switch --flake ~/NixOS/.#gaming --impure";
         cleanup = "nh clean all --keep 5";
         rift = "~/Rift/bin/rift";
+        albion = "~/Games/albion-online/data/Albion-Online";
         ".." = "cd ..";
         "/" = "cd /";
         "~" = "cd ~";
