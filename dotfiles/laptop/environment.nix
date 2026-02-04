31d5d298b89be74aa00d06d789daf49412fbeee4
CommanderBosko <kurthoernig@gmail.com>
2026-02-03 21:55:22 -0500

    moved timezone/keyboard to localisation.nix

--- dotfiles/laptop/environment.nix
+++ dotfiles/laptop/environment.nix
@@ -1,25 +1,6 @@
 { config, pkgs, system, ... }:
 
 {
-  # Time zone
-  time.timeZone = "America/New_York";
-
-  # Internationalisation
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
-
   # Services
   services = {
     # Desktop Environment
@@ -141,6 +122,9 @@
         fi
       '';
     };
+
+    # Xwayland
+    xwayland.enable = true;
 
     # AppImage support
     appimage = {
