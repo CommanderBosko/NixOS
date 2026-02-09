e6be026c83951438e30d280ffb2f77ef7d0cb248
CommanderBosko <kurthoernig@gmail.com>
2026-02-08 21:31:36 -0500

    taking first steps to home-manager

--- dotfiles/common/configs/home.nix
+++ dotfiles/common/configs/home.nix
@@ -1,12 +1,23 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, self, ... }:
 
 {
-
+  # Set home-manager state version
   home.stateVersion = "25.11";   # ← do NOT change this later — read the comment in home-manager release notes
 
-  # Quick smoke test packages
+  # Packages
   home.packages = with pkgs; [
+    kitty
+    starship
   ];
+
+  # Starship
+  programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
+
+  home.file.".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${self}/dotfiles/common/configs/starship.toml";
 
   # home.file.".config/some-app.conf".source = ./files/some-app.conf;
 
