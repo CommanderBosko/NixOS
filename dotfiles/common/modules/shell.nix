e5753fdc0f9970005bd653d7193c43b35bad54c7
CommanderBosko <kurthoernig@gmail.com>
2026-02-09 08:07:38 -0500

    3.0 complete

--- dotfiles/common/modules/shell.nix
+++ dotfiles/common/modules/shell.nix
@@ -5,52 +5,54 @@ let
 in
 {
   # Customize zsh
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
 
-    histSize = 10000;
-    histFile = "$HOME/.zsh_history";
-    setOptions = [ "HIST_IGNORE_ALL_DUPS" ];
+      histSize = 10000;
+      histFile = "$HOME/.zsh_history";
+      setOptions = [ "HIST_IGNORE_ALL_DUPS" ];
 
-    shellAliases = {
-      ".." = "cd ..";
-      "/" = "cd /";
-      "~" = "cd ~";
-      la = "ls -a";
-      ll = "ls -l";
-      lla = "ls -la";
-      edit = "sudo micro";
-      update = ''
-        echo ""
-        echo "Updating your system"
-        echo ""
-        sudo nix flake update --flake ~/NixOS/.
-        echo ""
-        echo "Updating Flatpaks"
-        flatpak update -y
-        echo ""
-        sudo nixos-rebuild switch --flake ~/NixOS/.#${hostName} --impure
-      '';
-      cleanup = ''
-        nh clean all --keep 5
-        echo ""
-        echo "Removing unused Flatpaks"
-        flatpak remove --unused --noninteractive
+      shellAliases = {
+        ".." = "cd ..";
+        "/" = "cd /";
+        "~" = "cd ~";
+        la = "ls -a";
+        ll = "ls -l";
+        lla = "ls -la";
+        edit = "sudo micro";
+        update = ''
+          echo ""
+          echo "Updating your system"
+          echo ""
+          sudo nix flake update --flake ~/NixOS/.
+          echo ""
+          echo "Updating Flatpaks"
+          flatpak update -y
+          echo ""
+          sudo nixos-rebuild switch --flake ~/NixOS/.#${hostName} --impure
+        '';
+        cleanup = ''
+          nh clean all --keep 5
+          echo ""
+          echo "Removing unused Flatpaks"
+          flatpak remove --unused --noninteractive
+        '';
+      };
+
+      # Pywal initation or not if no pywal cache generated
+      shellInit = ''
+        if [[ $- == *i* && -f ~/.cache/wal/sequences ]]; then
+          (cat ~/.cache/wal/sequences &)
+        fi
       '';
     };
 
     # Starship
     starship.enable = true;
-
-    # Pywal initation or not if no pywal cache generated
-    shellInit = ''
-      if [[ $- == *i* && -f ~/.cache/wal/sequences ]]; then
-        (cat ~/.cache/wal/sequences &)
-      fi
-    '';
   };
 
   # Shell packages
