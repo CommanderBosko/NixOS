267caf66a25ffd4ce130c86496a07ca08491f7be
CommanderBosko <kurthoernig@gmail.com>
2026-02-08 19:29:29 -0500

    added lm_sensors to shell.nix

--- dotfiles/gaming/users.nix
+++ dotfiles/gaming/users.nix
@@ -1,20 +1,45 @@
 { config, pkgs, ... }:
 
 {
-  # Definie user
+  # Definie users
+
+  # Bosko
   users.users.bosko = {
     shell = pkgs.zsh;
     isNormalUser = true;
     description = "bosko";
+    password = "password";
     extraGroups = [
+      "audio"
       "kvm"
       "libvirtd"
+      "lp"
       "networkmanager"
+      "video"
       "wheel"
     ];
 
     packages = with pkgs; [ ];
   };
 
-  nix.settings.trusted-users = [ "root" "bosko" ];
+  # Natty
+  users.users.natty = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    description = "natty";
+    password = "password";
+    extraGroups = [
+      "audio"
+      "kvm"
+      "libvirtd"
+      "lp"
+      "networkmanager"
+      "video"
+      "wheel"
+    ];
+
+    packages = with pkgs; [ ];
+  };
+
+  nix.settings.trusted-users = [ "root" "bosko" "natty" ];
 }
