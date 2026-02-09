8ce050d94e90fceee203654cdf5e8fd6625455d6
CommanderBosko <kurthoernig@gmail.com>
2026-02-08 21:31:36 -0500

    taking first steps to home-manager

--- dotfiles/common/modules/home-manager.nix
+++ dotfiles/common/modules/home-manager.nix
@@ -9,7 +9,6 @@
     extraSpecialArgs  = { inherit inputs; };   # optional
 
     users.bosko = import "${self}/dotfiles/common/configs/home.nix";   # or ./home.nix, whatever structure you like
+    users.natty= import "${self}/dotfiles/common/configs/home.nix";
   };
-
-#   environment.systemPackages = with pkgs; [ home-manager ];
 }
