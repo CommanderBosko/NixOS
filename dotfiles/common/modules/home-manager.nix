aed1b1691ee97c663a8211eba13ff489096ddce3
CommanderBosko <kurthoernig@gmail.com>
2026-02-08 21:44:38 -0500

    taking first steps to home-manager

--- dotfiles/common/modules/home-manager.nix
+++ dotfiles/common/modules/home-manager.nix
@@ -6,7 +6,7 @@
   home-manager = {
     useGlobalPkgs     = true;
     useUserPackages   = true;
-    extraSpecialArgs  = { inherit inputs; };   # optional
+    extraSpecialArgs  = { inherit inputs self; };   # optional
 
     users.bosko = import "${self}/dotfiles/common/configs/home.nix";   # or ./home.nix, whatever structure you like
     users.natty = import "${self}/dotfiles/common/configs/home.nix";
