376176fd2b0a4bc774774a9a9be23a41bf39cbb9
CommanderBosko <kurthoernig@gmail.com>
2026-02-12 07:06:48 -0500

    undoing last changes

--- dotfiles/common/modules/emulation.nix
+++ dotfiles/common/modules/emulation.nix
@@ -2,14 +2,7 @@
 
 {
   # Flatpak
-  services.flatpak = {
-    enable = true;
-    remotes = {
-        name = "flathub";
-        location = "https://flathub.org/repo/flathub.flatpakrepo";
-    };
-  };
-
+  services.flatpak.enable = true;
 
   programs = {
     # Xwayland
