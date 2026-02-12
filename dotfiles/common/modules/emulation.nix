137b5f3016e92768d0c2756ecb9e076aa3f46db3
CommanderBosko <kurthoernig@gmail.com>
2026-02-12 07:04:13 -0500

    adding flathub repo to emulation

--- dotfiles/common/modules/emulation.nix
+++ dotfiles/common/modules/emulation.nix
@@ -5,10 +5,8 @@
   services.flatpak = {
     enable = true;
     remotes = [
-      {
         name = "flathub";
         location = "https://flathub.org/repo/flathub.flatpakrepo";
-      }
     ];
   };
 
