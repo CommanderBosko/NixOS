87ca05f4d490768fd9750b8515f3cb8d80e22705
Kurt Hoernig <kurthoernig@gmail.com>
2026-02-15 20:16:48 -0500

    feat: Remove niri.nix module

--- flake.nix
+++ flake.nix
@@ -42,7 +42,6 @@
       "${self}/dotfiles/common/modules/audio.nix"
       "${self}/dotfiles/common/modules/emulation.nix"
       "${self}/dotfiles/common/modules/gaming.nix"
-      "${self}/dotfiles/common/modules/niri.nix"
       "${self}/dotfiles/common/modules/nvidia.nix"
       "${self}/dotfiles/common/modules/virtualisation.nix"
     ];
