79321f2e2b4f7df79a335b7b4f94dc9d43e6020a
Kurt Hoernig <kurthoernig@gmail.com>
2026-02-16 23:39:19 -0500

    refactor: Remove pantheon module from laptop config and re-enable defaultSession

---
+++ dotfiles/common/modules/desktop-environments/pantheon.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable X11 server (Pantheon is X11-native)
+  services.xserver.enable = true;
+
+  # Enable Pantheon desktop environment
+  services.desktopManager.pantheon.enable = true;
+
+  # Set Pantheon as the default session for the display manager
+  services.displayManager.defaultSession = "pantheon";
+
+  # Pantheon installs default applications. If specific ones are needed, they can be added.
+  # environment.systemPackages = with pkgs; [
+  #   pantheon.elementary-files # Pantheon's file manager
+  #   pantheon.pantheon-terminal # Pantheon's terminal
+  # ];
+}
