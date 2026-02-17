829541e12815c2fa6b551a0a46147d2a08714ede
Kurt Hoernig <kurthoernig@gmail.com>
2026-02-16 23:31:01 -0500

    refactor: Remove budgie module from laptop config after dry run

--- dotfiles/common/modules/desktop-environments/budgie.nix
+++ dotfiles/common/modules/desktop-environments/budgie.nix
@@ -5,13 +5,12 @@
   services.xserver.enable = true;
 
   # Enable Budgie desktop environment
-  services.xserver.desktopManager.budgie.enable = true;
+  services.desktopManager.budgie.enable = true;
 
   # Add common Budgie applications and utilities
   environment.systemPackages = with pkgs; [
     budgie-desktop # The main Budgie desktop package
     budgie-control-center # Budgie's settings panel
-    budgie-screenshot-applet # Screenshot applet
     # Add other desired Budgie applications here
   ];
 }
