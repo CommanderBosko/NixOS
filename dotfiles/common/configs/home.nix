ab37bf570f635e6d726dacdeede494f1e0db3890
Kurt Hoernig <kurthoernig@gmail.com>
2026-03-30 23:31:03 -0400

    alphabatized home.nix

--- dotfiles/common/configs/home.nix
+++ dotfiles/common/configs/home.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, self, ... }:
+{
+  pkgs,
+  lib,
+  self,
+  ...
+}:
 
 {
   home = {
@@ -8,13 +13,13 @@
     # Packages
     packages = with pkgs; [
       bash-language-server
-      shfmt
-      nodePackages.typescript-language-server
-      nodePackages.prettier
       jdt-language-server
+      lua-language-server
+      nodePackages.prettier
+      nodePackages.typescript-language-server
       pyright
       ruff
-      lua-language-server
+      shfmt
       stylua
     ];
 
@@ -39,83 +44,120 @@
     };
   };
 
-  # Customize helix
-  programs.helix = {
-    enable = true;
-    settings = {
-      theme = "autumn_night_transparent";
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
+  # Customize programs
+  programs = {
+    # Helix
+    helix = {
+      enable = true;
+      settings = {
+        theme = "autumn_night_transparent";
+        editor.cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
       };
-    };
 
-    languages.language = [
-      {
-        name = "nix";
-        scope = "source.nix";
-        file-types = ["nix"];
-        auto-format = true;
-        formatter.command = lib.getExe pkgs.nixfmt;
-        language-servers = [
-          "nixd"
-          "nil"
-        ];
-      }
-      {
-        name = "bash";
-        scope = "source.bash";
-        file-types = ["sh" "bash"];
-        auto-format = true;
-        formatter = { command = lib.getExe pkgs.shfmt; args = ["-i" "2"]; };
-        language-servers = [ "bash-language-server" ];
-      }
-      {
-        name = "zsh";
-        scope = "source.zsh";
-        file-types = ["zsh"];
-        auto-format = true;
-        formatter = { command = lib.getExe pkgs.shfmt; args = ["-i" "2"]; };
-        language-servers = [ "bash-language-server" ];
-      }
-      {
-        name = "javascript";
-        scope = "source.js";
-        file-types = ["js" "mjs" "cjs"];
-        auto-format = true;
-        formatter = { command = lib.getExe pkgs.nodePackages.prettier; args = ["--parser" "typescript"]; };
-        language-servers = [ "typescript-language-server" ];
-      }
-      {
-        name = "java";
-        scope = "source.java";
-        file-types = ["java"];
-        auto-format = true;
-        language-servers = [ "jdtls" ];
-      }
-      {
-        name = "python";
-        scope = "source.python";
-        file-types = ["py"];
-        auto-format = true;
-        formatter = { command = lib.getExe pkgs.ruff; args = ["format" "-"]; };
-        language-servers = [ "pyright" ];
-      }
-      {
-        name = "lua";
-        scope = "source.lua";
-        file-types = ["lua"];
-        auto-format = true;
-        formatter = { command = lib.getExe pkgs.stylua; args = ["-"]; };
-        language-servers = [ "lua-language-server" ];
-      }
-    ];
+      languages.language = [
+        {
+          name = "bash";
+          scope = "source.bash";
+          file-types = [
+            "sh"
+            "bash"
+          ];
+          auto-format = true;
+          formatter = {
+            command = lib.getExe pkgs.shfmt;
+            args = [
+              "-i"
+              "2"
+            ];
+          };
+          language-servers = [ "bash-language-server" ];
+        }
+        {
+          name = "java";
+          scope = "source.java";
+          file-types = [ "java" ];
+          auto-format = true;
+          language-servers = [ "jdtls" ];
+        }
+        {
+          name = "javascript";
+          scope = "source.js";
+          file-types = [
+            "js"
+            "mjs"
+            "cjs"
+          ];
+          auto-format = true;
+          formatter = {
+            command = lib.getExe pkgs.nodePackages.prettier;
+            args = [
+              "--parser"
+              "typescript"
+            ];
+          };
+          language-servers = [ "typescript-language-server" ];
+        }
+        {
+          name = "lua";
+          scope = "source.lua";
+          file-types = [ "lua" ];
+          auto-format = true;
+          formatter = {
+            command = lib.getExe pkgs.stylua;
+            args = [ "-" ];
+          };
+          language-servers = [ "lua-language-server" ];
+        }
+        {
+          name = "nix";
+          scope = "source.nix";
+          file-types = [ "nix" ];
+          auto-format = true;
+          formatter.command = lib.getExe pkgs.nixfmt;
+          language-servers = [
+            "nixd"
+            "nil"
+          ];
+        }
+        {
+          name = "python";
+          scope = "source.python";
+          file-types = [ "py" ];
+          auto-format = true;
+          formatter = {
+            command = lib.getExe pkgs.ruff;
+            args = [
+              "format"
+              "-"
+            ];
+          };
+          language-servers = [ "pyright" ];
+        }
+        {
+          name = "zsh";
+          scope = "source.zsh";
+          file-types = [ "zsh" ];
+          auto-format = true;
+          formatter = {
+            command = lib.getExe pkgs.shfmt;
+            args = [
+              "-i"
+              "2"
+            ];
+          };
+          language-servers = [ "bash-language-server" ];
+        }
+      ];
 
-    themes = {
-      autumn_night_transparent = {
-        "inherits" = "autumn_night";
-        "ui.background" = { };
+      themes = {
+        autumn_night_transparent = {
+          "inherits" = "autumn_night";
+          "ui.background" = { };
+        };
       };
     };
   };
